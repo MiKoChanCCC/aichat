9abde3f2ebe5b518fe79dd2a6977e1796362198d
MiKo Chan <mikochan@MiKodeMacBook-2.local>
2026-06-17 09:36:41 +0800

    创建路由

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import route from "./route/index.js";
+import { RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={route}>
+      <App />
+    </RouterProvider>
   </React.StrictMode>,
-)
+);
---
+++ src/pages/chatPage/ChaPage.jsx
@@ -0,0 +1,7 @@
+import "./chaPage.css";
+
+const ChaPage = () => {
+  return <div className="chaPage">ChaPage</div>;
+};
+
+export default ChaPage;
